218a12c7fefedfb342d2fc1e8f33da227715b8f4
Wesley Nunes Marques Torres <wesley.nmtorres@gmail.com>
2019-05-01 11:27:10 -0300

    include Docker to deploy app

--- siteware-e-commerce/siteware-rest/index.js
+++ siteware-e-commerce/siteware-rest/index.js
@@ -18,7 +18,7 @@ mongoose.Promise = bluebird;
 mongoose.connect(config.mongo.url, {useNewUrlParser: true});
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
